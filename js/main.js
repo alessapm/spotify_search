334039d0d0798bbd2edec927afc2a022c7ab89d1
Alessa Messineo <apmessineo@gmail.com>
2017-02-03 17:53:43 -0500

    pulling data from API, not adding to html

---
+++ js/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+$(document).ready(() => {
+console.log('js linked')
+
+const searchResults = $('#results-template').html();
+const searchTemplate = Handlebars.compile(searchResults);
+
+
+const searchSpotify = function(name, type){
+  $.ajax({
+    url: `https://api.spotify.com/v1/search?q=${name}&type=${type}`,
+    type: 'GET',
+    dataType: 'json'
+  })
+  .then((data) => {
+    // console.log('hello');
+    console.log(data)
+    $('#search-results').html(searchTemplate(data.artists.items))
+  })
+  .catch(err => console.log('error!'));
+}
+
+$('#user-input').on('keyup', (e) => {
+  console.log('hello')
+  let userInput = $('#user-input').val();
+  let dropdownSelection = $('#search-mode').val();
+  searchSpotify(userInput, dropdownSelection);
+});
+
+});
