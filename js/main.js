1c2b4f573445bd5ae44c60900aea4bddedf88aa0
Alessa Messineo <apmessineo@gmail.com>
2017-02-04 16:20:13 -0500

    tried a lot of things but back at the same place. Object loads but cannot add to my html

--- js/main.js
+++ js/main.js
@@ -1,33 +1,36 @@
 'use strict';
 
 $(document).ready(() => {
-console.log('js linked')
+  console.log('js linked!');
 
-const searchResults = $('#results-template').html();
-const searchTemplate = Handlebars.compile(searchResults);
+  const searchResults = $('#results-template').html();
+  const resultsTemplate = Handlebars.compile(searchResults);
 
-
-const searchSpotify = function(name, type){
-  $.ajax({
-    url: `https://api.spotify.com/v1/search?q=${name}&type=${type}`,
-    type: 'GET',
-    dataType: 'json'
-  })
-  .then((data) => {
-    // console.log('hello');
-    console.log(data);
-    $('#search-results').html(searchTemplate(data));
-
-
-  })
-  .catch(err => console.log('error!'));
-}
-
-$('#user-input').on('keyup', (e) => {
-  console.log('hello')
-  let userInput = $('#user-input').val();
   let dropdownSelection = $('#search-mode').val();
-  searchSpotify(userInput, dropdownSelection);
-});
+
+  //console.log(searchResults);
+
+  const searchSpotify = function(name,type) {
+    $.ajax({
+      url: `https://api.spotify.com/v1/search?q=${name}&type=${dropdownSelection}`,
+      type: 'GET',
+      dataType: 'json'
+    })
+    .then((data) => {
+      console.log(data);
+      $('#search-results').html(resultsTemplate(data));
+    })
+    .catch(err => console.log('error!'));
+  }
+
+  $('#user-input').on('keyup', (e) => {
+
+    let dropdownSelection = $('#search-mode').val();
+    let userInput = $('#user-input').val();
+
+    var newSearch = _.debounce(searchSpotify.bind(null, $(e.target).val(), dropdownSelection), 500)();
+  });
 
 });
+
+//data.artists.items.name
